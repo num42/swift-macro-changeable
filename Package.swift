aadf1a039ba0fbca27d0916a69bef17f6ce9f9c5
Wolfgang Lutz <wlut@num42.de>
2024-05-08 13:37:29 +0200

    run swiftformat

--- Package.swift
+++ Package.swift
@@ -7,45 +7,44 @@ import PackageDescription
 let name = "Changeable"
 
 let package = Package(
-  name: name,
-  platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
-  products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
-    .library(
-      name: name,
-      targets: [name]
-    )
-  ],
-  dependencies: [
-    .package(url: "https://github.com/num42/swift-macrotester.git", from: "1.0.1"),
-    .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1")
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
-    // Macro implementation that performs the source transformation of a macro.
-    .macro(
-      name: "\(name)Macros",
-      dependencies: [
-        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-        .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-      ]
-    ),
-    
-    // Library that exposes a macro as part of its API, which is used in client programs.
-    .target(
-      name: name,
-      dependencies: [.target(name: "\(name)Macros")]
-    ),
-    
-    // A test target used to develop the macro implementation.
-    .testTarget(
-      name: "\(name)Tests",
-      dependencies: [.target(name: "\(name)Macros"),
-                     .product(name: "MacroTester", package: "swift-macrotester"),
-                     .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
-      ],
-      resources: [.copy("Resources")]
-    )
-  ]
+    name: name,
+    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: name,
+            targets: [name]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/num42/swift-macrotester.git", from: "1.0.1"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "\(name)Macros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]
+        ),
+
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(
+            name: name,
+            dependencies: [.target(name: "\(name)Macros")]
+        ),
+
+        // A test target used to develop the macro implementation.
+        .testTarget(
+            name: "\(name)Tests",
+            dependencies: [.target(name: "\(name)Macros"),
+                           .product(name: "MacroTester", package: "swift-macrotester"),
+                           .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")],
+            resources: [.copy("Resources")]
+        ),
+    ]
 )
--- Tests/ChangeableTests/ChangeableTests.swift
+++ Tests/ChangeableTests/ChangeableTests.swift
@@ -1,19 +1,19 @@
 import ChangeableMacros
+import MacroTester
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 import XCTest
-import MacroTester
 
 let testMacros: [String: Macro.Type] = [
-  "Changeable": ChangeableFunctionMacro.self
+    "Changeable": ChangeableFunctionMacro.self,
 ]
 
 final class ChangeableFunctionMacroTests: XCTestCase {
-  func testChangeable() {
-    testMacro(macros: testMacros)
-  }
+    func testChangeable() {
+        testMacro(macros: testMacros)
+    }
 
-  func testChangeableWithComment() {
-    testMacro(macros: testMacros)
-  }
+    func testChangeableWithComment() {
+        testMacro(macros: testMacros)
+    }
 }
