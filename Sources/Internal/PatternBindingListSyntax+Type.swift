dc9b33ca0869b5d81053330dde5ac93c6fb75ca9
Wolfgang Lutz <wlut@num42.de>
2026-02-09 17:02:01 +0100

    format

--- Sources/Internal/PatternBindingListSyntax+Type.swift
+++ Sources/Internal/PatternBindingListSyntax+Type.swift
@@ -1,11 +1,1 @@
-import SwiftSyntax
-
-extension PatternBindingListSyntax.Element {
-  var type: String {
-    // remove any inlined comments after the type
-    typeAnnotation!.type.description
-      .components(separatedBy: "//")
-      .first!
-      .trimmingCharacters(in: .whitespaces)
-  }
-}
+../../../../MacroAccessHelpers.swift
